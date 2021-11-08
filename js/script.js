5a5ff19bac0ffb6a2878001571218931999472ef
Alex <adrdra88@gmail.com>
2021-11-08 16:01:16 +0100

    without css changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+// prima versione utilizzando classlist 
 
+// const aperto = document.getElementById('open');
+// const close = document.getElementById('close');
+
+// aperto.addEventListener('click', function(){
+//     document.querySelector('.hamburger-menu').classList.add('display-block');
+// });
+
+// close.addEventListener('click', function(){
+//     document.querySelector('.hamburger-menu').classList.remove('display-block');
+// });
+
+
+// seconda versione non toccando come, come da consegna, il css 
 
 const aperto = document.getElementById('open');
 const close = document.getElementById('close');
 
 aperto.addEventListener('click', function(){
-    document.querySelector('.hamburger-menu').classList.add('display-block')
-})
+    document.querySelector('.hamburger-menu').style.display = "block";
+});
 
 close.addEventListener('click', function(){
-    document.querySelector('.hamburger-menu').classList.remove('display-block')
-})
+    document.querySelector('.hamburger-menu').style.display = "none";
+});
